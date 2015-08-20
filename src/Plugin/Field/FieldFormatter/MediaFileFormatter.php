85912042f797e59238f82218538361f2a680b18f
aritnath1990 <arit.nath@dcplkolkata.com>
2015-08-20 21:16:59 +0530

    module update

--- src/Plugin/Field/FieldFormatter/MediaFileFormatter.php
+++ src/Plugin/Field/FieldFormatter/MediaFileFormatter.php
@@ -1,1 +1,39 @@
 <?php
+
+/**
+ * @file
+ * Contains \Drupal\image_title_caption\Plugin\Field\FieldFormatter\ImageTitleCaption.
+ */
+
+namespace Drupal\media_player_module\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\file\Plugin\Field\FieldFormatter;
+
+/**
+ * Plugin implementation of the 'image_title_caption' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "image_title_caption",
+ *   label = @Translation("Image with caption from title"),
+ *   field_types = {
+ *     "image"
+ *   }
+ * )
+ */
+class MediaFileFormatter extends GenericFileFormatter {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items) {
+    $elements = parent::viewElements($items);
+    foreach ($elements as &$element) {
+      $element['#theme'] = 'media_file_formatter';
+    }
+
+    return $elements;
+  }
+
+}
+
